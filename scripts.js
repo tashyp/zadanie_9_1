01adecbdd547ed2e87579ecea50e5124f887e358
Przemek Saja <przemek.saja@gmail.com>
2018-05-13 21:47:09 +0200

    Add index.html

---
+++ scripts.js
@@ -0,0 +1,16 @@
+//scripts.js
+
+function getTriangleArea(a, h) {
+	if ( (a <= 0) && (h <= 0) ) {
+    console.log('Incorrect data');
+} else if ( (a > 0) && (h > 0) ) {    
+    return(a*h/2);
+} else {
+    console.log('Data is ok');
+}
+}
+console.log(getTriangleArea(10, 6))
+
+var triangle1Area = getTriangleArea(10,15),
+	triangle2Area = getTriangleArea(8,13),
+	triangle3Area = getTriangleArea(5,10);
