7779cd4569bf0beb7d1c4eadca4259ad0f8f5959
Przemek Saja <przemek.saja@gmail.com>
2018-05-14 16:37:28 +0200

    Add changed scripts.js

--- scripts.js
+++ scripts.js
@@ -14,6 +14,6 @@ console.log('Data is ok');
 console.log(getTriangleArea(10, 6))
 
 var triangle1Area = getTriangleArea(10,15);
-var	triangle2Area = getTriangleArea(8,13);
-var	triangle3Area = getTriangleArea(5,10);
+var triangle2Area = getTriangleArea(8,13);
+var triangle3Area = getTriangleArea(5,10);
 
