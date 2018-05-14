6a9daf88e51e1ae1c3a8507372878696f015b506
Przemek Saja <przemek.saja@gmail.com>
2018-05-14 16:35:15 +0200

    Change scripts.js file

--- scripts.js
+++ scripts.js
@@ -1,16 +1,19 @@
 //scripts.js
 
 function getTriangleArea(a, h) {
-	if ( (a <= 0) && (h <= 0) ) {
-    console.log('Incorrect data');
-} else if ( (a > 0) && (h > 0) ) {    
-    return(a*h/2);
-} else {
-    console.log('Data is ok');
+if ( (a <= 0) && (h <= 0) ) {
+console.log('Incorrect data');
+} 
+else if ( (a > 0) && (h > 0) ) {    
+return(a*h/2);
+} 
+else {
+console.log('Data is ok');
 }
 }
 console.log(getTriangleArea(10, 6))
 
-var triangle1Area = getTriangleArea(10,15),
-	triangle2Area = getTriangleArea(8,13),
-	triangle3Area = getTriangleArea(5,10);
+var triangle1Area = getTriangleArea(10,15);
+var	triangle2Area = getTriangleArea(8,13);
+var	triangle3Area = getTriangleArea(5,10);
+
